e59d6559619a0d3d81e6ceff8ceb2f84624fa1da
My-QA <malharipatil7@gmail.com>
2025-08-24 18:59:46 +0530

    new changes

--- com_Web_Application_Automate/src/com/facebook/genericPage/MasterPage.java
+++ com_Web_Application_Automate/src/com/facebook/genericPage/MasterPage.java
@@ -1,9 +1,46 @@
 package com.facebook.genericPage;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class MasterPage {
 	
 	public static WebDriver driver;
-
+	public Properties prop;
+	public Properties or;
+	public Properties td;
+	
+	// constructor Implementation
+	public MasterPage() throws IOException {
+		
+		// Configuration properties file Implementation
+		FileInputStream ip = new FileInputStream(".\\src\\com\\facebook\\repository\\config.properties");
+		prop = new Properties();
+		prop.load(ip);
+		
+		// Locators properties file Implementation
+		FileInputStream fs = new FileInputStream(".\\src\\com\\facebook\\repository\\locators.properties");
+		or = new Properties();
+		or.load(fs);
+		
+		// testdata properties file Implementation
+		FileInputStream ts = new FileInputStream(".\\src\\com\\facebook\\repository\\testdata.properties");
+		td = new Properties();
+		td.load(ts);
+		
+		// Launching Browsers
+		if(prop.getProperty("browser").equalsIgnoreCase("chrome")){
+			System.setProperty("webdriver.chrome.driver", ".\\src\\com.facebook.drivers\\chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+		else if(prop.getProperty("browser").equalsIgnoreCase("edge")) {
+			System.setProperty("webdriver.edge.driver", ".src\\com.facebook.drivers\\edgedriver.exe");
+			driver= new EdgeDriver();
+		}
+     }
 }
